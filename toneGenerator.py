1c86ebf3eb297535f83643d112f85749f18b29d6
MartinSt80 <martin.stoeckl80@googlemail.com>
2021-10-23 22:55:24 +0200

    complete overhaul, functionality unchanged

--- toneGenerator.py
+++ toneGenerator.py
@@ -4,104 +4,114 @@ from tkinter import ttk
 import pygame
 import numpy as np
 
-pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=8192)
-root = Tk()
 
+class ToneGenerator:
 
-def SineObject(*args):
-	frames = np.linspace(0, 2 * math.pi, 44100)
-	values = []
-	for n in frames:
-		if len(args) == 1:
-			values.append(int(32767 * (math.sin(args[0] * n))))
+	def __init__(self):
+		pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=8192)
+
+		self.channel1_mixer = pygame.mixer.Channel(1)
+		self.channel2_mixer = pygame.mixer.Channel(2)
+
+		self.root = Tk()
+		self.root.title("Tone Generator")
+
+		self.channel1_freq = IntVar()
+		self.channel1_freq.set(400)
+		self.channel2_freq = IntVar()
+		self.channel2_freq.set(200)
+
+		self.sound_started = False
+
+		self.channel1_active = BooleanVar()
+		self.channel1_active.set(False)
+		self.channel2_active = BooleanVar()
+		self.channel2_active.set(False)
+
+		self.mainframe = self.__draw_mainframe()
+
+		self.channel1_frame = self.__draw_channelframe(self.channel1_active, self.channel1_freq)
+		self.channel1_frame.grid(column=0, row=0)
+		self.channel2_frame = self.__draw_channelframe(self.channel2_active, self.channel2_freq)
+		self.channel2_frame.grid(column=0, row=1)
+
+		self.play_button = self.__draw_buttonframe()
+
+		self.root.mainloop()
+
+	def __draw_mainframe(self):
+		mainframe = ttk.Frame(self.root, padding="3 3 12 12")
+		mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+		mainframe.columnconfigure(0, weight=1)
+		mainframe.rowconfigure(0, weight=1)
+		return mainframe
+
+	def __draw_channelframe(self, channel_active, channel_freq):
+		channel_frame = ttk.Frame(self.mainframe, padding="3 3 12 12")
+
+		channel_activebox = ttk.Checkbutton(channel_frame,
+											text='Activate Channel',
+											variable=channel_active,
+											command=self.__start_sound)
+		channel_activebox.grid(column=0, row=0, sticky=W)
+
+		channel1_display = ttk.Entry(channel_frame,
+									 text=channel_freq,
+									 textvariable=channel_freq,
+									 width=4)
+		channel1_display.bind('<Return>', self.__start_sound)
+		channel1_display.grid(column=1, row=0, sticky=E)
+
+		ttk.Label(channel_frame, text='Hz').grid(column=2, row=0, sticky=W)
+
+		channel_freqslider = ttk.Scale(channel_frame,
+									   from_=200,
+									   to=2000,
+									   length=300,
+									   value=channel_freq.get(),
+									   variable=channel_freq,
+									   command=self.__start_sound)
+		channel_freqslider.grid(column=0, row=1, columnspan=3)
+
+		return channel_frame
+
+	def __draw_buttonframe(self):
+		button_frame = ttk.Frame(self.mainframe, padding="3 3 12 12")
+		button_frame.grid(column=0, row=2)
+		play_button = ttk.Button(button_frame, text="Start", command=self.__start_pressed)
+		play_button.grid(column=0, row=0)
+		quit_button = ttk.Button(button_frame, text="Quit", command=self.root.destroy).grid(column=1, row=0)
+		return play_button
+
+	def __start_sound(self, event=None):
+		if self.sound_started:
+			if self.channel1_active.get():
+				self.channel1_mixer.play(self.__calc_sine_sound(self.channel1_freq.get()), loops=-1)
+			else:
+				self.channel1_mixer.stop()
+
+			if self.channel2_active.get():
+				self.channel2_mixer.play(self.__calc_sine_sound(self.channel2_freq.get()), loops=-1)
+			else:
+				self.channel2_mixer.stop()
+
+	def __start_pressed(self):
+		if self.play_button['text'] == 'Start':
+			self.sound_started = True
+			self.__start_sound()
+			self.play_button['text'] = 'Stop'
+
 		else:
-			values.append(int(32767 * (0.5 * math.sin(args[1] * n) + 0.5 * math.sin(args[0] * n))))
-	return pygame.mixer.Sound(np.array(values))
+			self.sound_started = False
+			pygame.mixer.stop()
+			self.play_button['text'] = 'Start'
+
+	def __calc_sine_sound(self, freq):
+		calc_sine = lambda x: int(32767 * (math.sin(freq * x)))
+		frames = np.linspace(0, 2 * math.pi, 44100)
+		values = np.array([calc_sine(x) for x in frames])
+		return pygame.mixer.Sound(values)
 
 
-def checkChannel():
-	if CHANNEL1_IS_ACTIVE.get() or CHANNEL2_IS_ACTIVE.get():
-		startSound()
-	else:
-		snd1.stop()
-		snd2.stop()
-
-
-def startpressed():
-	global START_IS_PRESSED
-	START_IS_PRESSED = True
-	checkChannel()
-
-def stoppressed():
-	global START_IS_PRESSED
-	START_IS_PRESSED = False
-	pygame.mixer.stop()
-
-def startSound(*args):
-	if START_IS_PRESSED:
-		if CHANNEL1_IS_ACTIVE.get():
-			snd1.play(SineObject(CHANNEL1_SETFREQ.get()), loops=-1)
-		if CHANNEL2_IS_ACTIVE.get():
-			snd2.play(SineObject(CHANNEL2_SETFREQ.get()), loops=-1)
-
-
-def state_of_play():
-	if not CHANNEL1_IS_ACTIVE.get() and not CHANNEL2_IS_ACTIVE.get():
-		play_button.state(["disabled"])
-	else:
-		play_button.state(["!disabled"])
-
-
-snd1 = pygame.mixer.Channel(1)
-snd2 = pygame.mixer.Channel(2)
-
-
-CHANNEL1_SETFREQ = IntVar()
-CHANNEL1_SETFREQ.set(400)
-CHANNEL2_SETFREQ = IntVar()
-CHANNEL2_SETFREQ.set(200)
-
-START_IS_PRESSED = False
-STOP_IS_PRESSED = False
-
-CHANNEL1_IS_ACTIVE = BooleanVar()
-CHANNEL1_IS_ACTIVE.set(False)
-CHANNEL2_IS_ACTIVE = BooleanVar()
-CHANNEL2_IS_ACTIVE.set(False)
-
-root.title("Tone Generator")
-
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
-
-channel1_frame = ttk.Frame(mainframe, padding="3 3 12 12")
-channel1_frame.grid(column=0, row=0)
-channel1_activebox = ttk.Checkbutton(channel1_frame, text='Channel 1 Active', variable=CHANNEL1_IS_ACTIVE, command=checkChannel)
-channel1_activebox.grid(column=0, row=0, sticky=W)
-channel1_display = ttk.Entry(channel1_frame, text=CHANNEL1_SETFREQ, textvariable=CHANNEL1_SETFREQ, width=4)
-channel1_display.bind('<Return>', startSound)
-channel1_display.grid(column=1, row=0,sticky=E)
-ttk.Label(channel1_frame, text='Hz').grid(column=2, row=0, sticky=W)
-channel1_freqslider = ttk.Scale(channel1_frame, from_=200, to=6000, length=300, value=CHANNEL1_SETFREQ.get(), variable=CHANNEL1_SETFREQ, command=startSound)
-channel1_freqslider.grid(column=0, row=1, columnspan=3)
-
-channel2_frame = ttk.Frame(mainframe, padding="3 3 12 12")
-channel2_frame.grid(column=0, row=1)
-channel2_activebox = ttk.Checkbutton(channel2_frame, text='Channel 2 Active', variable=CHANNEL2_IS_ACTIVE, command=checkChannel)
-channel2_activebox.grid(column=0, row=0, sticky=W)
-channel2_display = ttk.Entry(channel2_frame, text=CHANNEL2_SETFREQ, textvariable=CHANNEL2_SETFREQ, width=4)
-channel2_display.bind('<Return>', startSound)
-channel2_display.grid(column=1, row=0,sticky=E)
-ttk.Label(channel2_frame, text='Hz').grid(column=2, row=0, sticky=W)
-channel2_freqslider = ttk.Scale(channel2_frame, from_=200, to=6000, length=300, value=CHANNEL2_SETFREQ.get(), variable=CHANNEL2_SETFREQ, command=startSound)
-channel2_freqslider.grid(column=0, row=1, columnspan=3)
-
-play_button = ttk.Button(mainframe, text="Start", command=startpressed).grid(column=1, row=3)
-stop_button = ttk.Button(mainframe, text="Stop", command=stoppressed).grid(column=2, row=3)
-quit_button = ttk.Button(mainframe, text="Quit", command=root.destroy).grid(column=3, row=3)
-
-root.mainloop()
-
+ToneGenerator()
 
